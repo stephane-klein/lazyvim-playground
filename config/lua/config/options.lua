6df2a44ceaa9b63e0f445c492ce81086cda75668
Stéphane Klein <contact@stephane-klein.info>
2025-12-24 17:35:49 +0100

    Enable foldmethod marker for Lua files

--- config/lua/config/options.lua
+++ config/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = "*.lua",
+  callback = function()
+    vim.defer_fn(function()
+      vim.wo.foldmethod = "marker"
+      vim.wo.foldmarker = "{{{,}}}"
+    end, 100)
+  end,
+})
