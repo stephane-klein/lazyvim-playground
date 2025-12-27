1becb1a7b1b2889cae89b3de4b5c57d681c24fd9
Stéphane Klein <contact@stephane-klein.info>
2025-12-27 10:58:55 +0100

    Configuration of my keymaps for moving between windows and their split

--- config/lua/config/keymaps.lua
+++ config/lua/config/keymaps.lua
@@ -2,6 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Remove some LazyVim default keymaps {{{
+
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+
+-- Windows
+vim.keymap.del("n", "<leader>-")
+vim.keymap.del("n", "<leader>|")
+
+-- better up/down
+vim.keymap.del({ "n", "x" }, "j")
+vim.keymap.del({ "n", "x" }, "k")
+
+-- }}}
+
 -- cutlass.nvim equivalent {{{
 -- Delete/change operations don't yank (use "m" for cut/move instead)
 vim.keymap.set({ "n", "x" }, "d", '"_d', { desc = "Delete without yanking" })
@@ -20,4 +38,19 @@ vim.keymap.set({ "n", "x" }, "M", "D", { desc = "Cut to end of line (move)" })
 -- Preserve marks functionality with "gm"
 vim.keymap.set("n", "gm", "m", { desc = "Set mark" })
 
+-- }}}
+
+-- Move to windows and split windows {{{
+vim.keymap.set("n", "<Leader><Up>", "<C-W><C-K>", { desc = "Move to up window" })
+vim.keymap.set("n", "<Leader><Left>", "<C-W><C-H>", { desc = "Move to left window" })
+vim.keymap.set("n", "<Leader><Right>", "<C-W><C-L>", { desc = "Move to right window" })
+vim.keymap.set("n", "<Leader><Down>", "<C-W><C-J>", { desc = "Move to down window" })
+
+vim.keymap.set("n", "<Leader><Leader><Up>", "<Cmd>:sp<CR><C-W><C-K>", { desc = "Split on the top" })
+vim.keymap.set("n", "<Leader><Leader><Left>", "<Cmd>:vs<CR><C-W><C-H>", { desc = "Split on the left" })
+vim.keymap.set("n", "<Leader><Leader><Right>", "<Cmd>:vs<CR>", { desc = "Split on the right" })
+vim.keymap.set("n", "<Leader><Leader><Down>", "<Cmd>:sp<CR>", { desc = "Split on the bottom" })
+
+vim.keymap.set("n", "<Leader><Leader>c", "<C-W><C-C>", { desc = "Close window" })
+vim.keymap.set("n", "<Leader><Leader>z", function() Snacks.zen.zoom() end, { desc = "Zoom window" })
 -- }}}
