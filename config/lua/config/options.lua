24c8bc5679994741b603d9b0098d43465e5c8552
Stéphane Klein <contact@stephane-klein.info>
2025-12-28 11:35:06 +0100

    Disable some annoying features like swapfile
    For example, when I open the same file in multiple terminals, I don't want
    to be bothered by swap warnings. I'm willing to risk losing some changes.

--- config/lua/config/options.lua
+++ config/lua/config/options.lua
@@ -11,3 +11,19 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     end, 100)
   end,
 })
+-- Disable some annoying features like swapfile {{{
+-- Disable swap files
+vim.opt.swapfile = false
+
+-- Disable backup files (file~)
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+-- Keep undo history but centralize it (useful feature to keep)
+local undo_dir = vim.fn.stdpath('state') .. '/undo'
+if vim.fn.isdirectory(undo_dir) == 0 then
+  vim.fn.mkdir(undo_dir, 'p')
+end
+vim.opt.undofile = true
+vim.opt.undodir = undo_dir
+-- }}
