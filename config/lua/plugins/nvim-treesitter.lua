8f00e4450c67ec701126b8a8822988ba380441fa
Stéphane Klein <contact@stephane-klein.info>
2025-12-28 11:35:06 +0100

    Enable Aerial for Mise and Golang files

--- config/lua/plugins/nvim-treesitter.lua
+++ config/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
+      "toml",
+    })
+
     -- Register the custom predicate
     require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
       local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
