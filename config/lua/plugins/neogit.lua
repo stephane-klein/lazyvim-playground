181b91e7c1fd52c35e5e3fd9eb28c65128889e30
Stéphane Klein <contact@stephane-klein.info>
2026-01-22 22:36:05 +0100

    Install Neogit plugin (instead Fugitive)

---
+++ config/lua/plugins/neogit.lua
@@ -0,0 +1,13 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "folke/snacks.nvim"
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  }
+}
